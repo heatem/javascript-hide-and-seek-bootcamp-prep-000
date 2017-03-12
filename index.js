e886ac4ffd7ac39188bebeb13175ca0a79e92d9b
Heather Mason <heatem@gmail.com>
2017-03-12 02:24:18 +0000

    Done.

--- index.js
+++ index.js
@@ -11,14 +11,34 @@ function getFirstSelector(selector) {
 // right? :) ). (Note that in `index.html` `#nested` and `.target`
 // just _happen_ to be `div`s. This method should work with arbitrary elements.)
 function nestedTarget() {
-  const target = document.querySelector(body #nested .target);
+  const target = document.querySelector('body #nested .target');
   return target;
 }
 
+// - Define a function `increaseRankBy(n)`
+// that increases the ranks in all of the `.ranked-list`s by `n`.
+// (You might need to make use of [`parseInt()`]
+// (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt)
+function increaseRankBy(n) {
+  const ranks = document.querySelectorAll('.ranked-list');
+  for (let i = 0; i < ranks.length; i++) {
+    var rank = parseInt(ranks[i]);
+    ranks[i].innerHTML = (rank + 1).toString();
+  }
+}
 
-
-// - Define a function `increaseRankBy(n)` that increases the ranks in all of the `.ranked-list`s by `n`. (You might need to make use of [`parseInt()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt)
 // - Define a function `deepestChild()` that pulls out the most deeply nested child
 // from `div#grand-node`. (Remember, you can iterate over elements and call
 // `querySelector()` and `querySelectorAll()` on them. This is challenging to
 // implement correctly, but not beyond your ability!)
+function deepestChild() {
+  var node = document.getElementById('grand-node');
+  var nextNode = node.children[0];
+
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+
+  return node;
+}
